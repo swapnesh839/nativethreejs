c25d6dbfc199be5780c438dce748f0a0957a9662
swapnesh <swapnesha839@gmail.com>
2024-08-25 00:22:04 +0530

    v2

--- main.js
+++ main.js
@@ -1,24 +1,72 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from "three";
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js"
+const scene = new THREE.Scene
+const canvas = document.getElementById("app")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const textureloader = new THREE.TextureLoader
 
-setupCounter(document.querySelector('#counter'))
+// const geometry = new THREE.BoxGeometry(1,1,1)
+// const material = new THREE.MeshBasicMaterial({color:"red"})
+// const Cube = new THREE.Mesh(geometry,material)
+// Cube.rotation.set()
+// scene.add(Cube)
+// Cube.position.normalize()
+
+// console.log(Cube.position.distanceTo(Camera.position))
+// console.log(Cube.position.distanceTo(Camera.position))
+
+// const spherematerial =new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+// const spheregeometry =  new THREE.SphereGeometry(1, 32, 32)
+// const sphere = new THREE.Mesh(spheregeometry,spherematerial)
+// scene.add(sphere)
+
+
+const planematerial = new THREE.MeshBasicMaterial({
+  color:"purple",
+  side:THREE.DoubleSide
+})
+const planegeometry = new THREE.PlaneGeometry(1,1)
+const planetexture = textureloader.load("https://images.pexels.com/photos/235985/pexels-photo-235985.jpeg?auto=compress&cs=tinysrgb&w=400") 
+const plane = new THREE.Mesh(planegeometry,planematerial)
+scene.add(plane)
+
+
+// const point = new THREE.Vector2()
+
+
+
+
+const Camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,.1,1000)
+
+Camera.position.set(-1,1,3)
+Camera.position.x= 1
+// Camera.rotateY()
+scene.add(Camera)
+
+
+const renderer = new THREE.WebGLRenderer({
+  canvas:canvas,
+  antialias:true
+})
+// renderer.setClearColor("red")
+const controler = new OrbitControls(Camera,renderer.domElement)
+controler.rotateSpeed=1.3
+const setuporbitalcontrol= ()=>{
+  requestAnimationFrame(setuporbitalcontrol)
+  controler.update()
+  renderer.render(scene,Camera)
+}
+setuporbitalcontrol()
+//ambientlight
+const ambientlight = new THREE.AmbientLight("white",1)// color ,intensity,distance,decay
+// ambientlight.position = Camera.position
+scene.add(ambientlight)
+
+//directionallight
+const directionallight1 = new THREE.DirectionalLight("white",1)
+directionallight1.position.set(1,3,4)
+scene.add(directionallight1)
+
+renderer.setSize(window.innerWidth,window.innerHeight)
+renderer.render(scene,Camera)
