85c40a0e8e13143884effbf43292061ab8355eb6
swapnesh <swapnesha839@gmail.com>
2024-08-25 21:41:43 +0530

    update

--- Tween.js
+++ Tween.js
@@ -1,14 +1,19 @@
 import * as THREE from "three";
-import * as TWEEN from "@tweenjs/tween.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // Create the scene
 const scene = new THREE.Scene();
-
 // Set up the camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(1, 3, 3);
+camera.position.set(0, 7, 1);
 scene.add(camera);
+
+
+// axesHelper
+const axesHelper = new THREE.AxesHelper(5); 
+scene.add(axesHelper);
+const axesHelper2 = new THREE.AxesHelper(-5); 
+scene.add(axesHelper2);
 
 // Set up the renderer
 const canvas = document.getElementById("app");
@@ -16,23 +21,35 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   antialias: true,
 });
+
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
-// Set up the controls
+
+
+// Set up the orbitalcontrols
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.rotateSpeed = 0.9;
+const animate = () => {
+  requestAnimationFrame(animate)
+  controls.update()
+  renderer.render(scene, camera);
+}
+animate()
 
 // Create box meshes
 const box1 = new THREE.Mesh(
-  new THREE.BoxGeometry(5, 0.1, 5),
-  new THREE.MeshBasicMaterial({ color: "red" })
+  new THREE.BoxGeometry(9, 0.1, 7),
+  new THREE.MeshStandardMaterial({ color: "red" })
 );
 const box2 = new THREE.Mesh(
   new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial({ color: "green" })
+  new THREE.MeshStandardMaterial({ color: "green" })
 );
 box1.position.y = -0.5;
+//ulternative
+// box1.position.set(0, -0.5, 0
+//Default is 0,0,0
 scene.add(box1);
 scene.add(box2);
 
@@ -45,19 +62,58 @@ const directionalLight1 = new THREE.DirectionalLight("white", 1);
 directionalLight1.position.set(1, 3, 4);
 scene.add(directionalLight1);
 
-// Set up Tween
-const transition = new TWEEN.Tween({ x: 0, y: 0, z: 0 })
-  .to({ x: 1, y: 0, z: 0 }, 2000)
-  .onUpdate((e) => {
-    box2.position.x = e.x;
-  })
-  .start();
 
-// Combined animation loop
-const animate = (time) => {
-  requestAnimationFrame(animate);
-  TWEEN.update(time);
-  controls.update();
-  renderer.render(scene, camera);
-};
-animate();
+
+//manual movement on event
+document.onkeydown = checkKey;
+function checkKey(e) {
+
+  if (e.keyCode == '38') {
+    box2.position.z -= 0.1
+  }
+  else if (e.keyCode == '40') {
+    box2.position.z += 0.1
+  }
+  else if (e.keyCode == '37') {
+    box2.position.x -= 0.1
+  }
+  else if (e.keyCode == '39') {
+    box2.position.x += 0.1
+  }
+  requestAnimationFrame(() => {
+    renderer.render(scene, camera);
+  })
+
+}
+
+renderer.render(scene, camera);
+
+
+//manual rotation
+const rotationrange = document.getElementById("Rotation")
+rotationrange.addEventListener("input", (e) => {
+  requestAnimationFrame(() => {
+    box2.rotation.y = rotationrange.value
+  })
+})
+
+//manual color change
+const Boxcolor = document.getElementById("Boxcolor")
+Boxcolor.addEventListener("input", (e) => {
+  requestAnimationFrame(() => {
+    box2.material.color.set(Boxcolor.value)
+  })
+})
+
+//mamual light strength
+const Lights = document.getElementById("Lights")
+Lights.addEventListener("input", (e) => {
+
+  if (Lights.checked) {
+    ambientLight.intensity = 1
+    directionalLight1.intensity = 1
+  } else {
+    ambientLight.intensity = 0
+    directionalLight1.intensity = 0
+  }
+})
